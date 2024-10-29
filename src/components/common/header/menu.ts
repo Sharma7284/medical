37781582e88e22f94fc4cc175754f38220d60edf
Ravi Sharma <ravi701107@gmail.com>
2024-10-29 14:01:57 +0530

    Add heaer

---
+++ src/components/common/header/menu.ts
@@ -0,0 +1,31 @@
+const HeaderMenu = [
+  {
+    label: "Treatment",
+  },
+  {
+    label: "Wellness",
+  },
+  {
+    label: "Doctors",
+  },
+  {
+    label: "Hospitals",
+    isDropdown: true,
+    items: [
+      {
+        label: "Hospital 01",
+      },
+      {
+        label: "Hospital 02",
+      },
+    ],
+  },
+  {
+    label: "Blogs",
+  },
+  {
+    label: "Join as a Partner",
+  },
+];
+
+export default HeaderMenu;
